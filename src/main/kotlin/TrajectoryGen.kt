ebfc00b40e1002a8d08a7f80dc2437399ee96b3f
AdamMeilicke <meiliada000@edmonds15.org>
2021-09-25 14:49:04 -0700

    Deleted the old paths, mechanisms, and field, and created a new basic path

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -10,113 +10,40 @@ object TrajectoryGen {
 
     val color = Color.BLUE
 
-    private val startPose = Pose2d(-63.0, 48.0.y, 0.0.toRadians)
-
-    private val powerShotPose = listOf(Vector2d(72.0, 20.0), Vector2d(72.0, 12.0), Vector2d(72.0, 1.0))
-
-    private val towerPose = Vector2d(72.0, 26.0)
-
-    // number of degrees the shooter shoots offcenter
-    private const val OFFSET = 14.3
+    private val startPose = Pose2d(-36.0, 63.0, 180.0.toRadians)
 
     private val drive = TrajectoryBuild(DriveConstantsComp)
 
-    private val mech = MechanismController()
-
-    // travel to drop zone, drop wobble goal between movements, prepare to shoot rings
-    private val startToLow = drive.trajectoryBuilder(startPose, startPose.heading)
-        .splineTo(Vector2d(3.0, 52.0.y), 320.0.a.flip.toRadians)
-        .build()
-    private val startToMid = drive.trajectoryBuilder(startPose, startPose.heading)
-        .splineTo(Vector2d(17.0, 40.0), 270.0.toRadians)
-        .build()
-    private val startToHigh = drive.trajectoryBuilder(startPose, startPose.heading + 20.0.toRadians)
-        .splineToConstantHeading(Vector2d(58.0, 44.0), 0.0.toRadians)
+    private val startToCarousel = drive.trajectoryBuilder(startPose, startPose.heading)
+        .splineToConstantHeading(Vector2d(-53.5, 62.0), 175.0.toRadians)
         .build()
 
-    private val lowToPowershot = drive.trajectoryBuilder(startToLow.end(), startToLow.end().heading - 90.0.toRadians)
-        .splineToLinearHeading(Pose2d(-7.0, 26.0, powerShotAngle(Vector2d(-7.0, 26.0), 0)), 270.0.toRadians)
-        .build()
-    private val midToPowershot = drive.trajectoryBuilder(startToMid.end(), startToMid.end().heading - 90.0.toRadians)
-        .splineToLinearHeading(Pose2d(-7.0, 26.0, powerShotAngle(Vector2d(-7.0, 26.0), 0)), 270.0.toRadians)
-        .build()
-    private val highToPowershot = drive.trajectoryBuilder(startToHigh.end(), startToHigh.end().heading - 135.0.toRadians)
-        .splineToLinearHeading(Pose2d(-7.0, 26.0, powerShotAngle(Vector2d(-7.0, 26.0), 0)), 90.0.toRadians)
+    private val carouselToPark = drive.trajectoryBuilder(startToCarousel.end(), startToCarousel.end().heading)
+        .back(95.0)
         .build()
 
-    private val powershotToWobble = drive.trajectoryBuilder(Pose2d(lowToPowershot.end().vec(), powerShotAngle(lowToPowershot.end().vec(), 2)), powerShotAngle(midToPowershot.end().vec(), 2))
-        .splineTo(Vector2d(-20.0, 34.0), 135.0.toRadians)
-        .splineTo(Vector2d(-48.0, 41.0), 180.0.toRadians)
-        .build()
-    private val powershotToRingToWobble = drive.trajectoryBuilder(Pose2d(midToPowershot.end().vec(), powerShotAngle(midToPowershot.end().vec(), 2)), powerShotAngle(midToPowershot.end().vec(), 2))
-        .splineTo(Vector2d(-20.0, 34.0), 135.0.toRadians)
-        .splineTo(Vector2d(-48.0, 41.0), 180.0.toRadians)
-        .build()
-
-    private val powershotToAboveRings = drive.trajectoryBuilder(Pose2d(highToPowershot.end().vec(), powerShotAngle(highToPowershot.end().vec(), 2)), powerShotAngle(highToPowershot.end().vec(), 2) - 90.0.toRadians)
-        .splineTo(Vector2d(-7.0, 39.5), 90.0.toRadians)
-        .build()
-    private val aboveRingsToRings = drive.trajectoryBuilder(powershotToAboveRings.end(), powershotToAboveRings.end().heading)
-        .lineToConstantHeading(Vector2d(-34.0, 39.5))
-        .build()
-    private val ringsToRingsFurther = drive.trajectoryBuilder(aboveRingsToRings.end(), aboveRingsToRings.end().heading)
-        .splineToConstantHeading(Vector2d(-44.0, 39.5), 180.0.toRadians)
-        .build()
-
-    private val wobbleToShootTower =
-        drive.trajectoryBuilder(powershotToWobble.end(), true)
-            .splineTo(Vector2d(-10.0, 32.0), towerAngle(Vector2d(-10.0, 32.0)) + 180.0.toRadians)
-            .build()
-
-    private val wobbleToLow =
-        drive.trajectoryBuilder(powershotToWobble.end(), true)
-            .splineToLinearHeading(Pose2d(12.0, 46.0.y, 0.0.toRadians), 320.0.a.flip.toRadians)
-            .build()
-
-    private val lowToPark =
-        drive.trajectoryBuilder(wobbleToLow.end(), wobbleToLow.end().heading - 90.0.toRadians)
-            .splineTo(Vector2d(12.0, 28.0.y), 270.0.a.toRadians)
-            .build()
-    private val ringsToPark =
-        drive.trajectoryBuilder(Pose2d(ringsToRingsFurther.end().vec(), towerAngle(ringsToRingsFurther.end().vec())), true)
-            .splineTo(Vector2d(10.0, 28.0.y), 0.0.a.toRadians)
-            .build()
-
-    private val towerToMidToPark =
-        drive.trajectoryBuilder(wobbleToShootTower.end(), true)
-            .splineTo(Vector2d(16.0, 38.0), 90.0.toRadians)
-            .build()
 
     fun createTrajectory(): ArrayList<Trajectory> {
-        return createTrajectoryC()
+        return arrayListOf(startToCarousel, carouselToPark)
     }
 
-    fun createTrajectories() {
-
+    // Shipping Element / Duck in the Left position
+    private fun createTrajectoryLeft(): ArrayList<Trajectory> {
+        return arrayListOf()
     }
 
-    private fun createTrajectoryA(): ArrayList<Trajectory> {
-        return arrayListOf(startToLow, lowToPowershot, powershotToWobble, wobbleToLow, lowToPark)
+    // Shipping Element / Duck in the Middle position
+    private fun createTrajectoryMiddle(): ArrayList<Trajectory> {
+        return arrayListOf()
     }
 
-    private fun createTrajectoryB(): ArrayList<Trajectory> {
-        return arrayListOf(startToMid, midToPowershot, powershotToRingToWobble, wobbleToShootTower, towerToMidToPark)
-    }
-
-    private fun createTrajectoryC(): ArrayList<Trajectory> {
-        return arrayListOf(startToHigh, highToPowershot, powershotToAboveRings, aboveRingsToRings, ringsToRingsFurther, ringsToPark)
+    // Shipping Element / Duck in the Right position
+    private fun createTrajectoryRight(): ArrayList<Trajectory> {
+        return arrayListOf()
     }
 
     fun drawOffbounds() {
         GraphicsUtil.fillRect(Vector2d(0.0, -63.0), 18.0, 18.0) // robot against the wall
-    }
-
-    private fun towerAngle(position: Vector2d): Double {
-        return (position - towerPose angleBetween Vector2d(1.0, 0.0)) + OFFSET.toRadians
-    }
-
-    private fun powerShotAngle(position: Vector2d, num: Int): Double {
-        return (position - powerShotPose[num] angleBetween Vector2d(1.0, 0.0)) + OFFSET.toRadians
     }
 }
 
